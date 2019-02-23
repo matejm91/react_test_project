b02684f3dc764878a5c0ddabb74ccbbeada08d8c
Matej Marić <maric.matej91@gmail.com>
2019-02-23 16:04:47 +0100

    Remove ref

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
           >
             Learn React
           </a>
