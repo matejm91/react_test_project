c54afae90495bfd30abd06f8082b8b5a6c4d856e
Dasina <thedasa33@gmail.com>
2019-02-27 13:25:46 +0100

    Add Header.css in Header.js scripts

---
+++ client/src/PageBase/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+
+        );
+    }
+}
+
+export default Header;
