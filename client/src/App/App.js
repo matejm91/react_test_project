aa526b15bde15de7e63b78fa3789ac97baac7e7f
Dasina <thedasa33@gmail.com>
2019-02-27 13:24:34 +0100

    Header and footer scripts added to App.js

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Header from '../PageBase/Header';
+import Footer from '../PageBase/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
