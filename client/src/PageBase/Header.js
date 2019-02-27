7fb652270c790a843b5f3a1fed0ee470553b3f91
Dasina <thedasa33@gmail.com>
2019-02-27 15:32:36 +0100

    changed text in elements

--- client/src/PageBase/Header.js
+++ client/src/PageBase/Header.js
@@ -5,10 +5,10 @@ class Header extends Component {
   render() {
     return (
       <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
+        <li><a href="#Naslovna stranica">Naslovna stranica </a></li>
+        <li><a href="#Novosti"> Novosti </a></li>
+        <li><a href="#Kontakt"> Kontakt </a></li>
+        <li><a href="#O nama"> O nama</a></li>
       </ul>
     );
   }
