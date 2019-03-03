19e3ccd310e1367fac2490d3ab81b853727d6460
Matej Marić <maric.matej91@gmail.com>
2019-03-03 13:48:07 +0100

    Add about us 2

---
+++ client/src/AboutUs/aboutus2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    console.log('about us Page');
+    return <p>about us</p>;
+  }
+}
+
+export default AboutPage;
