f1b3a90d84e6a6478227cb838a293453df62d0d3
Matej Marić <maric.matej91@gmail.com>
2019-03-04 20:15:58 +0100

    Add and use flexbox grid in contact page

--- client/src/Contacts/contactPage.js
+++ client/src/Contacts/contactPage.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import './contacts.css';
+
 class ContactPage extends React.Component {
   render() {
-    console.log('contact Page: ', this.props);
-    return <p>contact</p>;
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-1"></div>
+          <div className="col-xs-6 contactDiv">
+            <div>
+              Adresa: Ulica Pere Kvržice 15, Novi Zagreb, Botinec
+            </div>
+            <div>
+              Radno vrijeme župnog ureda: 08:00-09:00 i 16:30-18:00.
+            </div>
+            <div>
+              Za termine izvan gore navedenog vremena moguć je dogovor.
+            </div>
+            <div>
+              Telefon: 01 6545 877.
+            </div>
+            <div>
+              E-mail: rktzupabotinec@gmail.com
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'flexboxgrid2/flexboxgrid2.css';
 
 import './index.css';
+import './PageLayout/Header.css';
 import * as serviceWorker from './serviceWorker';
 import routes from './routes';
 import store from './redux/store';
 
-console.log('routes: ', routes);
 ReactDOM.render(
   <Provider store={store}>
     <div>
