d5680a97b4bd08387d9c6a926d134ba9e69013d6
Dasina <thedasa33@gmail.com>
2019-03-06 16:24:03 +0100

    add text and links

--- client/src/PageLayout/Footer.js
+++ client/src/PageLayout/Footer.js
@@ -4,7 +4,22 @@ import './Footer.css';
 export default function Footer(props) {
   return (
     <div className="footer">
-      <p>Footer</p>
+      <div>
+        <b>Korisne poveznice:</b>
+        <br></br>
+        <a href="http://w2.vatican.va/content/vatican/it.html"> Sveta stolica</a>
+        <br></br>
+        <a href="http://www.zg-nadbiskupija.hr/"> Zagrebacka nadbiskupija </a>
+        <br></br>
+        <a href="http://studentski-pastoral.com/"> Studentski pastoral </a>
+        <br></br>
+        <a href="https://www.glas-koncila.hr/"> Glas koncila </a>
+        <br></br>
+        <a href="http://www.laudato.hr/"> Laudato </a>
+        </div>
+        <div>
+          Info
+        </div>
     </div>
   );
 }
