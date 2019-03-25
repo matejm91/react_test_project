c86fc2b1ef839b7dad5b0fe495b54ad9e909d57a
Dasina <thedasa33@gmail.com>
2019-03-25 16:24:03 +0100

    add tag mailto

--- client/src/Contacts/contactPage.js
+++ client/src/Contacts/contactPage.js
@@ -15,7 +15,7 @@ class ContactPage extends React.Component {
               <b>Radno vrijeme župnog ureda:</b> 08:00-09:00 i 16:30-18:00. <br></br>
               <em>Za termine izvan gore navedenog vremena moguć je dogovor.</em> <br></br>
               <b>Telefon:</b> 01 6545 877. <br></br>
-              <b>E-mail:</b> rktzupabotinec@gmail.com
+              <b>E-mail:</b> <a href="mailto:rktzupabotinec@gmail.com">rktzupabotinec@gmail.com</a>
             </p>
           </bodyr2>
         </bodyr>
