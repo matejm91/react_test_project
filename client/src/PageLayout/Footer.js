30bbaa7a1284ba37c230eccae2d9c87888abc462
Dasina <thedasa33@gmail.com>
2019-03-28 11:03:59 +0100

    edit whitespace

--- client/src/PageLayout/Footer.js
+++ client/src/PageLayout/Footer.js
@@ -8,7 +8,7 @@ export default function Footer(props) {
       <div className='col-xs-4'>
         <div className="footer">
           <div>
-          <b>Korisne poveznice:</b>
+            <b>Korisne poveznice:</b>
             <br></br>
             <a href="http://w2.vatican.va/content/vatican/it.html"> Sveta stolica</a>
             <br></br>
@@ -27,37 +27,37 @@ export default function Footer(props) {
           <b>Info:</b>
         </div>
         <div>
-        Crkva sv. Stjepana Prvomučenika
+          Crkva sv. Stjepana Prvomučenika
         <br></br>
-         <div>
-           Uica Pere Kvržice 15
-         10020 Zagreb, Botinec
+          <div>
+            Uica Pere Kvržice 15
+          10020 Zagreb, Botinec
          </div>
         </div>
         <div>
-        <b>Telefon:</b> <br></br> 01 6545 877
+          <b>Telefon:</b> <br></br> 01 6545 877
         </div>
         <div>
           <b>E-mail:</b>
           <adress>
-          <br></br>
-          <a href="mailto:rktzupabotinec@gmail.com">rktzupabotinec@gmail.com</a>
-          <br></br>
-          <a href="mailto:nanovorodeni@gmail.com">nanovorodeni@gmail.com</a>
+            <br></br>
+            <a href="mailto:rktzupabotinec@gmail.com">rktzupabotinec@gmail.com</a>
+            <br></br>
+            <a href="mailto:nanovorodeni@gmail.com">nanovorodeni@gmail.com</a>
           </adress>
         </div>
       </div>
-    <div className='col-xs-4'>
-    <div className='footer'>
-    <b>Pratite nas:</b>
+      <div className='col-xs-4'>
+        <div className='footer'>
+          <b>Pratite nas:</b>
+        </div>
+        <div>
+          <a href="https://hr-hr.facebook.com/pages/category/Community/Nanovo-ro%C4%91eni-1046403725381309/"> Nanovo rođeni facebook</a>
+          <br></br>
+          <a href="https://www.youtube.com/channel/UCiXXRMlzoA-MKPHVe5V7YdQ"> Nanovo rođeni youtube </a>
+        </div>
+      </div>
     </div>
-    <div>
-    <a href="https://hr-hr.facebook.com/pages/category/Community/Nanovo-ro%C4%91eni-1046403725381309/"> Nanovo rođeni facebook</a>
-    <br></br>
-    <a href="https://www.youtube.com/channel/UCiXXRMlzoA-MKPHVe5V7YdQ"> Nanovo rođeni youtube </a>
-    </div>
-    </div>
-    </div>
-    
+
   );
 }
