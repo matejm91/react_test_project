0b74fa931f461a8e8eb8e2e7495bc4230ec480aa
Dasina <thedasa33@gmail.com>
2019-03-28 14:35:09 +0100

    add text and image

--- client/src/Home/homePage.js
+++ client/src/Home/homePage.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class HomePage extends React.Component {
   render() {
-    return <p>home</p>;
+    return (
+
+      <div>
+        <img src="http://zupabotinec.com/wp-content/uploads/2018/02/G5EaMqN.jpg" alt="Župa Botinec" width="1920" height="650"></img>
+        <p><b><em>Ne boj se stado malo!<br></br> Svidjelo se Ocu vašemu dati vam baštinu nebesku!!</em></b></p>
+      </div>
+    );
   }
 }
 
