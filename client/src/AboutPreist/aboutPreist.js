8ec6ecd059da6c788efbd98bdfe9223e1ef9e8dc
Matej <matej@bonsai.tech>
2019-07-07 17:15:17 +0200

    Add about preist folder and page layout with content

---
+++ client/src/AboutPreist/aboutPreist.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+class AboutPreist extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-xs-1" />
+        <div className="col-xs-4">
+          Rođen sam 9. svibnja 1965. u Zagrebu od oca Nikole i majke Barice rođ.
+          Crnković. Djetinjstvo sam proveo do svoje pete godine u Prečkom, a
+          ponekad sam boravio na selu kod djedova i baka na Žumberku (župe Gorje
+          Prekrižje i Gornji Oštrc). Od pete do devete godine živio sam s
+          roditeljima u Kanadi, u Torontu, kao i mnogi Hrvati koji su, zbog
+          politike koja je bila protiv Hrvatske i Hrvata, morali poći trbuhom za
+          kruhom. Na moje veliko inzistiranje, čime se jako ponosim, moji su
+          roditelji odlučili da se vratimo u Hrvatsku. Još kao dijete govorio
+          sam im da ću, kada odrastem, svakako vratiti se u Hrvatsku i tamo
+          živjeti, da ne kanim ostati u Kanadi, iako mi je tamo bilo lijepo. U
+          Kanadi sam završio prva tri razreda osnovne škole, i time po Božjoj
+          providnosti izbjegao pionirsku zakletvu koja se polagala u trećem
+          razredu. Po povratku iz Kanade, u četvrtom razredu postao sam
+          automatski predsjednik razreda. Nastavio sam, dakle, živjeti u svojoj
+          rodnoj župi – Prečkom, gdje sam završio osnovnu školu. Završio sam
+          zatim Klasičnu gimnaziju u Križanićevoj, čiji mi je klasični odgoj
+          otvorio vidike i dao slobodan duh. Nakon srednje škole išao sam na
+          Teologiju u Zagrebu jer sam osjećao svećenički poziv, za što su mi
+          mnogi ljudi pomogli. More ljudi je razgovaralo sa mnom o mome pozivu,
+          što bračnih parova, što pojedinaca, čak i kardinal Kuharić u jednom
+          razgovoru te profesorica Damjanov koja nije bila vjernica, nego je
+          vjerovala u tadašnji režim, svojim mi je savjetom pomogla da se
+          odlučim za duhovni poziv i donesem jednu od, ako ne i najvažniju
+          odluku, u životu.
+          <br />
+          Đakonsko ređenje bilo je 26.6.1988. Đakonsku službu vršio sam u
+          Velikoj Gorici, kod vlč. Frkina (prvog župnika u Botincu), od kojeg
+          sam najviše naučio o pastoralu obitelji i braka, temeljnih stvari
+          jedne župe i društva u cijelosti; i kasnije kod vlč. Matije Stepinca u
+          župi sv. Petra u Zagrebu, kod kojeg sam usavršio kancelarijski život.
+          Svećeničko ređenje bilo je 25.6.1989. Zanimljivo je da sam ređen na
+          isti dan kad i kardinal Bozanić (doduše on za biskupa, ja za
+          svećenika), a Bog nas je kasnije opet povezao u životu, sad je moj
+          nadbiskup. Od toga dana bio sam kapelan u Selskoj, u župi sv. Marka
+          Križevčanina, kod župnika Franje Juraka čiji je moto župnog pastorala
+          bio: „Ništa nas ne smije iznenaditi!“. O njemu je Kršćanska sadašnjost
+          izdala knjigu “Župnik u gradu”, koja opisuje njegov život, a u kojoj
+          je objavljen i moj govor “Život kapelana sa župnikom Jurakom”. Prvi je
+          župnik iz Zagreba o kojem je napisana i objavljena knjiga, što
+          svjedoči o njegovoj veličini i humanitarnim akcijama. Zahvalan sam mu
+          za sve što me je naučio, ali najviše na tome što me usmjerio prema
+          radu s mladima, koji mi sada uljepšavaju život.
+          <br />
+          Od 1994. do 1999. bio sam župnik u župi Draganec blizu Čazme, gdje me
+          zateklo vrlo loše stanje. Kada sam došao, u stanu je bilo devet
+          štakorskih legla. Za moje službe u Dragancu župni stan je obnovljen,
+          ali u tom obnovljenom župnom stanu nisam proveo ni jedan dan službe
+          jer sam premješten u drugu župu. Od 1999. do 2009. župnik sam bio u
+          rodnom mjestu braće Radić, župi Martinska Ves kod Siska. 11. lipnja,
+          kada bi se u tom mjestu obilježavao spomen na braću Radić, dolazio bi
+          državni vrh tako da sam se često družio i s političarima raznih
+          opcija. U ovoj župi župni stan je bio neprikladan za život, tako da je
+          za moje službe sagrađen novi od temelja.
+          <br />
+          Od 2009. do danas živim i djelujem u župi Botinec.
+          <br />
+          Projekt na koji sam najviše ponosan je svakako Hod za nerođene, akcija
+          koju sam započeo u Martinskoj Vesi, a nastavio u Botincu. Bez
+          razmišljanja kažem da mi je to najdraži projekt jer sa sigurnošću
+          možemo reći da su našom akcijom spašeni životi osam beba, za čiji je
+          abortus već bio uplaćen iznos od 800 kuna. To je krvavo teška borba,
+          borba za život, priče su zaista nevjerojatne, ali svjedočanstva majki
+          koje su odustale od pobačaja su snažna. Također, pokušavam mlade u
+          župi s kojima rado surađujem potaknuti na prihvaćanje duhovnog poziva
+          te im primjerom svog života pokazati ljepotu poziva.
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AboutPreist;
