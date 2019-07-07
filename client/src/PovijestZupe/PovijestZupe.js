37980362b14de4de9ca5f679eec920869b36b2ed
Matej <matej@bonsai.tech>
2019-07-07 16:54:27 +0200

    Format document

--- client/src/PovijestZupe/PovijestZupe.js
+++ client/src/PovijestZupe/PovijestZupe.js
@@ -7,27 +7,37 @@ class PovijestZupe extends React.Component {
     return (
       <div>
         <div className="row">
-          <div className="col-xs-1"></div>
+          <div className="col-xs-1" />
           <div className="col-xs-4 povijestDiv">
-            <div>
-              <b>Povijest Župe</b>
-            </div>
-            <div>
-              Naselje Botinec nastalo je nakon velike poplave koja je pogodila Zagreb u listopadu 1964.
-                Na to su područje preseljeni građani čije su kuće stradale u poplavi te su upravo oni bili prvi župljani naše župe.
-                Župa sv. Stjepana Prvomučenika osnovana je na blagdan Gospe od sv. Krunice 7. listopada 1969.
-                Prvi je župnik bio Leopold Tepeš. Iste godine, u župu dolaze časne sestre Družbe Kraljice svijeta koje u župi djeluju i danas.
-                Krajem te iste godine župna je crkva zazidana od strane komunističke vlasti
-                i tek na spomendan sv. Barbare 4. prosinca 1971. odzidana je izrazitim zalaganjem novog tadašnjeg župnika,
-                velečasnog Josipa Frkina.
-                Župa danas broji oko 5 tisuća župljana, od kojih najveći dio živi u Botincu, a manji u Starom Botincu i Dupcima.
-            </div>
+            <p>
+              <h3>
+                <a
+                  href="povijest-zupe"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Povijest Župe
+                </a>
+              </h3>
+              Naselje Botinec nastalo je nakon velike poplave koja je pogodila
+              Zagreb u listopadu 1964. Na to su područje preseljeni građani čije
+              su kuće stradale u poplavi te su upravo oni bili prvi župljani
+              naše župe. Župa sv. Stjepana Prvomučenika osnovana je na blagdan
+              Gospe od sv. Krunice 7. listopada 1969.<br />
+              Prvi je župnik bio Leopold Tepeš. Iste godine, u župu dolaze časne
+              sestre Družbe Kraljice svijeta koje u župi djeluju i danas.<br />
+              Krajem te iste godine župna je crkva zazidana od strane
+              komunističke vlasti i tek na spomendan sv. Barbare 4. prosinca
+              1971. odzidana je izrazitim zalaganjem novog tadašnjeg župnika,
+              velečasnog Josipa Frkina.<br />
+              Župa danas broji oko 5 tisuća župljana, od kojih najveći dio živi
+              u Botincu, a manji u Starom Botincu i Dupcima.
+            </p>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default PovijestZupe;
