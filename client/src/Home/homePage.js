2a01e1ea76898318865826feb546dc71999539c4
Matej <matej@bonsai.tech>
2019-07-07 16:52:02 +0200

    Change image size to fit screen size

--- client/src/Home/homePage.js
+++ client/src/Home/homePage.js
@@ -3,10 +3,21 @@ import React from 'react';
 class HomePage extends React.Component {
   render() {
     return (
-
       <div>
-        <img src="http://zupabotinec.com/wp-content/uploads/2018/02/G5EaMqN.jpg" alt="Župa Botinec" width="1920" height="650"></img>
-        <p><b><em>Ne boj se stado malo!<br></br> Svidjelo se Ocu vašemu dati vam baštinu nebesku!!</em></b></p>
+        <img
+          src="http://zupabotinec.com/wp-content/uploads/2018/02/G5EaMqN.jpg"
+          alt="Župa Botinec"
+          width="100%"
+          height="650"
+        />
+        <p>
+          <b>
+            <em>
+              Ne boj se stado malo!
+              <br /> Svidjelo se Ocu vašemu dati vam baštinu nebesku!!
+            </em>
+          </b>
+        </p>
       </div>
     );
   }
