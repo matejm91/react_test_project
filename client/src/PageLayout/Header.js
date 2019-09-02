1977a2ee8ce0c24e9ab3358c02937b32512acc07
matejm91 <maric.matej91@gmail.com>
2019-09-02 10:33:44 +0200

    Add buttons and react router links instead of hrefs

--- client/src/PageLayout/Header.js
+++ client/src/PageLayout/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
 import './Header.css';
 
 class Header extends Component {
@@ -7,23 +9,31 @@ class Header extends Component {
     return (
       <ul>
         <li>
-          <Button variant="contained" color="secondary" href="/">
-            Naslovnica
+          <Button variant="contained" color="secondary">
+            <Link to="/">
+              Naslovnica
+            </Link>
           </Button>
         </li>
         <li>
-          <Button variant="contained" color="secondary" href="about_us">
-            Župa
+          <Button variant="contained" color="secondary">
+            <Link to="about_us">
+              Župa
+            </Link>
           </Button>
         </li>
         <li>
-          <Button variant="contained" color="secondary" href="news">
-            Nanovo Rođeni
+          <Button variant="contained" color="secondary">
+            <Link to="news">
+              Nanovo Rođeni
+            </Link>
           </Button>
         </li>
         <li>
-          <Button variant="contained" color="secondary" href="contact">
-            Kontakt
+          <Button variant="contained" color="secondary">
+            <Link to="contact">
+              Kontakt
+            </Link>
           </Button>
         </li>
       </ul>
